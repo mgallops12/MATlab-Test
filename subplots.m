0407f0f2a886939da65b85d9113e1eec8f4c11bd
mgallops12 <116749167+mgallops12@users.noreply.github.com>
2022-11-09 12:57:11 -0500

    Add files via upload

---
+++ subplots.m
@@ -0,0 +1,21 @@
+subplot(2,2,1)
+x = linspace(0,10);
+y1 = sin(x);
+plot(x,y1)
+title('Subplot 1: sin(x)')
+
+subplot(2,2,2)
+y2 = sin(2*x);
+plot(x,y2)
+title('Subplot 2: sin(2x)')
+
+subplot(2,2,3)
+y3 = sin(4*x);
+plot(x,y3)
+title('Subplot 3: sin(4x)')
+
+subplot(2,2,4)
+y4 = sin(8*x);
+plot(x,y4)
+title('Subplot 4: sin(8x)')
+title('Subplots')
